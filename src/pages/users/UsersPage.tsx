2abdc80492df66407be20a881e0c2d88e84b98cb
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-09-05 16:41:59 +0300

    created a user page

---
+++ src/pages/users/UsersPage.tsx
@@ -0,0 +1,8 @@
+export default function UsersPage() {
+  return (
+    <section>
+      <h1 style={{ fontSize: 24, marginBottom: 12 }}>Users</h1>
+      <p>Burada kullanıcı listesini ve CRUD işlemlerini yapacağız.</p>
+    </section>
+  );
+}
