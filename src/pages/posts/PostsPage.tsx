9b37346e8aaf01f7c8ac0ac4ed14ae8f37fd1df3
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-09-05 16:42:15 +0300

    created a post page

---
+++ src/pages/posts/PostsPage.tsx
@@ -0,0 +1,8 @@
+export default function PostsPage() {
+  return (
+    <section>
+      <h1 style={{ fontSize: 24, marginBottom: 12 }}>Posts</h1>
+      <p>Burada post listesini ve CRUD işlemlerini yapacağız.</p>
+    </section>
+  );
+}
