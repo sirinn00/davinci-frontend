076558cde7bb5e981a07b60142f1ef019b217145
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-09-05 19:57:40 +0300

    used container component for homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import Container from "./components/Container";
 
 export default function App() {
   return (
     <div style={{ minHeight: "100vh", background: "#f6f7f9" }}>
       <NavBar />
-      <main style={{ width: "100%", padding: "24px 16px" }}>
-        <Outlet />
+      <main style={{ padding: "24px 0" }}>
+        <Container>
+          <Outlet />
+        </Container>
       </main>
     </div>
   );
 }
-
