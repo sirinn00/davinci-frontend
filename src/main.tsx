1708f78bcca38f9cc5e393d9fe653ab927e1258c
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-09-09 13:30:43 +0300

    added comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// Router
+import { StrictMode } from "react";                        
+import { createRoot } from "react-dom/client";             
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
-import Home from "./pages/Home";
-import UsersPage from "./pages/users/UsersPage";
-import PostsPage from "./pages/posts/PostsPage";
-import "./index.css";
+import Home from "./pages/Home";                           
+import UsersPage from "./pages/users/UsersPage";            
+import PostsPage from "./pages/posts/PostsPage";            
+import "./index.css";                                    
 
 const router = createBrowserRouter([
   {
+   
     path: "/",
     element: <App />,
     children: [
@@ -24,4 +26,5 @@ createRoot(document.getElementById("root")!).render(
     <RouterProvider router={router} />
   </StrictMode>
 );
+
 
